285e6f81297ef99cd8e9530036bff9ae9bb5954d
hmzaib <118712017+hmzaib@users.noreply.github.com>
2023-03-17 11:10:57 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+public class MyClass{
+    public static void Main()
+    {
+        var name="zaiba";
+        Console.WriteLine($"Hello world this is {name}");
+        Console.ReadKey();
+    }
+}
